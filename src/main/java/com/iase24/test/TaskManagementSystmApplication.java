ff35ed84dd5220859e91b29f061a492f9c7c3f5c
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-16 17:54:32 +0300

    git init

---
+++ src/main/java/com/iase24/test/TaskManagementSystmApplication.java
@@ -0,0 +1,13 @@
+package com.iase24.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iase24/test/TaskManagementSystmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iase24.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
