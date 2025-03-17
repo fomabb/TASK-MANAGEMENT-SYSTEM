3d38a848510b20dba1575b86c1d147e36c063f66
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-17 17:02:29 +0300

    refactoring method create

--- src/main/java/com/iase24/test/controller/AdminController.java
+++ src/main/java/com/iase24/test/controller/AdminController.java
@@ -1,9 +1,17 @@
 package com.iase24.test.controller;
 
+import com.iase24.test.dto.request.CreateTaskRequest;
+import com.iase24.test.dto.response.CreatedTaskResponse;
+import com.iase24.test.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminController {
 
+    private final TaskService taskService;
+
+    @Operation(
+            summary = "Создание новой задачи.",
+            description = "`В тело запроса необходимо добавить название, описание и приоритет.`"
+    )
+    @PostMapping
+    public ResponseEntity<CreatedTaskResponse> createTask(@RequestBody CreateTaskRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(request));
+    }
 }
