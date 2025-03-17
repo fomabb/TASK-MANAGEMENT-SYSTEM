ba2edf015ca516fc7992a62a249fb53bc7358502
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-17 19:48:57 +0300

    add test app.prop

--- src/test/java/com/iase24/test/TaskManagementSystemApplicationTests.java
+++ src/test/java/com/iase24/test/TaskManagementSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.iase24.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.location=classpath:/application.properties")
 class TaskManagementSystemApplicationTests {
 
     @Test
