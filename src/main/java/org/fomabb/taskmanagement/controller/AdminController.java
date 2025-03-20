7d32d580773f522cc21bbb16e8ecbd1c2724dfb1
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-20 17:10:15 +0300

    add description methods to README.md

--- src/main/java/org/fomabb/taskmanagement/controller/AdminController.java
+++ src/main/java/org/fomabb/taskmanagement/controller/AdminController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -252,7 +251,7 @@ public class AdminController {
                     )
             }
     )
-    @PutMapping("/comments/update")
+    @PatchMapping("/comments/update")
     public ResponseEntity<UpdateCommentResponse> updateComment(@RequestBody UpdateCommentRequest request) {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(commentService.updateComment(request));
     }
